7c65866fbe69aaa9d2b996a834a9e1760c5d25f2
4k5h1t <ask160203@gmail.com~>
2023-05-19 07:55:30 +0530

    cool

--- movierec/asgi.py
+++ movierec/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for MRS project.
+ASGI config for movierec project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MRS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movierec.settings')
 
 application = get_asgi_application()
